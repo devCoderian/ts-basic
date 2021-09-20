eb63c2c14bf4f025b7e57e99bf3f39019b9e2328
devCoderian <devCoderian@gmail.com>
2021-09-20 20:02:19 +0900

    tuple

---
+++ tuple.ts
@@ -0,0 +1,11 @@
+let x: [string, number];
+
+x = ["hello", 39];
+
+//x = [10, 'Mark']; error
+
+//x[2] = "world"; //튜플은 2자리 인데 3번째에 뭘 넣으려 함
+
+const person:[string, number] = ["Mark", 39 ];
+const [first, second] = person; 
+//const [first, second, thrid] = person; 길이가 안맞음
