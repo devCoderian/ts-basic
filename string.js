f7f7867ca291e2006c17b59faaf11654da2643f2
devCoderian <devCoderian@gmail.com>
2021-09-20 19:19:55 +0900

    string

---
+++ string.js
@@ -0,0 +1,5 @@
+"use strict";
+let myname = "mark";
+let age = 25;
+let sentense = `${myname}은${age}살이다.`
+console.log(sentense);
---
+++ string.ts
@@ -0,0 +1,5 @@
+let myname:string = "mark"
+let age:number = 25
+
+let sentense:string = `${myname}은${age}살이다.`
+console.log(sentense);
