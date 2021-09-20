f13374114aeacc43b7cba64169ecbd7c24d5e86e
devCoderian <devCoderian@gmail.com>
2021-09-20 20:19:52 +0900

    unknown

---
+++ unknown.ts
@@ -0,0 +1,15 @@
+declare const maybe:unknown;
+
+//const aNumber: number = maybe;
+
+if(maybe === true){
+   const aBoolean:boolean = maybe;
+
+   // const aString: string = maybe;
+}
+
+if(typeof maybe === "string"){
+    const aString: string = maybe;
+}
+
+//런타입에서 지정된다.
